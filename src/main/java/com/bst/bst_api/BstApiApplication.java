972d664c603f2418798fcd13cf61471800aada60
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-08-26 14:21:20 -0400

    API set-up

---
+++ src/main/java/com/bst/bst_api/BstApiApplication.java
@@ -0,0 +1,13 @@
+package com.bst.bst_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstApiApplication.class, args);
+	}
+
+}
